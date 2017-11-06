c75a83c65cbcb3ab9419f97416904fcfe0704e87
Lucy Block <Lucy@Nathan-Bridgess-iMac-6.local>
2017-11-06 14:35:33 -0500

    added longer time interval for more data

--- dashboard/sql/recent_sales.sql
+++ dashboard/sql/recent_sales.sql
@@ -61,7 +61,7 @@ INNER JOIN rentstab ON rentstab.ucbbl = pluto.bbl
 LEFT JOIN hpd_registrations_grouped_by_bbl_with_contacts hpd_reg on hpd_reg.bbl = pluto.bbl
 WHERE pluto.cd is not null
       AND pluto.cd = '${ cd }'
-      AND sales.saledate >= date_trunc('month', current_date - interval '2 month')
+      AND sales.saledate >= date_trunc('month', current_date - interval '3 month')
       AND sales.residentialunits > 0
       AND sales.saleprice > 50000
       AND COALESCE(uc2007,uc2008, uc2009, uc2010, uc2011, uc2012, uc2013, uc2014,uc2015,uc2016) is not null
