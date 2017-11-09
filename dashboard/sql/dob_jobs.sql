f392e2d52cfe69e571d7041712e1dc853acc13a4
Lucy Block <Lucy@Nathan-Bridgess-iMac-6.local>
2017-11-09 16:27:17 -0500

    changed time intervals for new json testing

--- dashboard/sql/dob_jobs.sql
+++ dashboard/sql/dob_jobs.sql
@@ -68,8 +68,7 @@ INNER JOIN rentstab rentstab on rentstab.ucbbl = dobjobs.bbl
 LEFT JOIN hpd_registrations_grouped_by_bbl_with_contacts hpd_reg on hpd_reg.bbl = dobjobs.bbl
 WHERE
    pluto.cd = '${ cd }' and
-   latestactiondate >= date_trunc('month', current_date - interval '3 month') and
-   latestactiondate <= date_trunc('month', current_date - interval '2 month')
+   latestactiondate >= date_trunc('month', current_date - interval '1 month') 
    AND coalesce(uc2007,uc2008, uc2009, uc2010, uc2011, uc2012, uc2013, uc2014,uc2015,uc2016) is not null
 group by pluto.cd, 
 		 dobjobs.bbl, 
