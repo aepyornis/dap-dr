b4aa9a51c6301293672cc4395dc568c48eef35a8
Lucy Block <Lucy@Nathan-Bridgess-iMac-6.local>
2017-11-22 12:36:30 -0500

    sql updates and svg test

--- dashboard/sql/dob_jobs.sql
+++ dashboard/sql/dob_jobs.sql
@@ -1,37 +1,39 @@
-SELECT
-	pluto.cd,
+SELECT subset.*,   
+	first(concat('<a href="http://whoownswhat.justfix.nyc/address/',
+       case 
+              when borocode = '1' then 'MANHATTAN'
+              when borocode = '2' then 'BRONX'
+              when borocode = '3' then 'BROOKLYN'
+              when borocode = '4' then 'QUEENS'
+              when borocode = '5' then 'STATEN ISLAND'
+       end,
+       '/',
+       split_part(address,' ',1),
+       '/',
+       split_part(address,' ',2),
+       '%20',
+       split_part(address,' ',3),
+       '%20',
+       split_part(address,' ',4),
+       '" target="_blank">',
+       replace(trim(both'"{}"' from cast(corpnames as text)), '"',''),
+        ' </a>'
+           )) as owner
+    from
+	(select pluto.cd,
 	dobjobs.bbl,
-	first(pluto.address) as address,
-	first(pluto.unitsres) as residentialunits,
+	pluto.address,
+	pluto.unitsres as residentialunits,
 	uc2007, uc2016,
-	count(case when dobjobs.jobtype ='A1' then 1 else null end) as a1,
-	count(case when dobjobs.jobtype='A2' then 1 else null end) as a2,
-	count(case when dobjobs.jobtype='DM' then 1 else null end) as dm,
-	sum(case
-		when dobjobs.jobtype = 'A1' then 1
-		when dobjobs.jobtype = 'A2' then 1
-		when dobjobs.jobtype = 'DM' then 1
-		else 0 end) as total,
-      concat('<a href="http://whoownswhat.justfix.nyc/address/',
-            case 
-                  when pluto.borocode = '1' then 'MANHATTAN'
-                  when pluto.borocode = '2' then 'BRONX'
-                  when pluto.borocode = '3' then 'BROOKLYN'
-                  when pluto.borocode = '4' then 'QUEENS'
-                  when pluto.borocode = '5' then 'STATEN ISLAND'
-             end,
-            '/',
-            split_part(pluto.address,' ',1),
-            '/',
-            split_part(pluto.address,' ',2),
-            '%20',
-            split_part(pluto.address,' ',3),
-            '%20',
-            split_part(pluto.address,' ',4),
-            '" target="_blank">',
-            replace(trim(both'"{}"' from cast(hpd_reg.corpnames as text)), '"',''),
-            ' </a>'
-               ) as owner,
+    pluto.borocode,
+    count(case when dobjobs.jobtype ='A1' then 1 else null end) as a1,
+    count(case when dobjobs.jobtype='A2' then 1 else null end) as a2,
+    count(case when dobjobs.jobtype='DM' then 1 else null end) as dm,
+    sum(case
+        when dobjobs.jobtype = 'A1' then 1
+        when dobjobs.jobtype = 'A2' then 1
+        when dobjobs.jobtype = 'DM' then 1
+        else 0 end) as total,
       concat('<a href="https://hpdonline.hpdnyc.org/HPDonline/Provide_address.aspx?p1=',
             pluto.borocode,
             '&p2=',
@@ -58,32 +60,19 @@ SELECT
             '&lot=',
             pluto.lot,
             '" target="_blank">(ACRIS)</a>'
-               ) as acrislink,
-      case when ((cast(uc2007 as float) - 
-                cast(uc2016 as float)) 
-               /cast(uc2007 as float) >= 0.25) then 0 else 1 end as highloss
-FROM dobjobs
-LEFT JOIN pluto_16v2 pluto on dobjobs.bbl = pluto.bbl
-INNER JOIN rentstab rentstab on rentstab.ucbbl = dobjobs.bbl
-LEFT JOIN hpd_registrations_grouped_by_bbl_with_contacts hpd_reg on hpd_reg.bbl = dobjobs.bbl
-WHERE
-   pluto.cd = '${ cd }' and
-   latestactiondate >= date_trunc('month', current_date - interval '1 month') 
-   AND coalesce(uc2007,uc2008, uc2009, uc2010, uc2011, uc2012, uc2013, uc2014,uc2015,uc2016) is not null
-group by pluto.cd, 
-		 dobjobs.bbl, 
-         pluto.address, 
-         pluto.unitsres, 
-         uc2007, 
-         uc2016, 
-         corpnames, 
-         pluto.borocode, 
-         pluto.block, 
-         pluto.lot
-HAVING ( sum(case
-		when dobjobs.jobtype = 'A1' then 1
-		when dobjobs.jobtype = 'A2' then 1
-		when dobjobs.jobtype = 'DM' then 1
-		else 0 end) > 0 )
-order by total desc
+               ) as acrislink
+	from dobjobs
+    LEFT JOIN pluto_16v2 pluto on dobjobs.bbl = pluto.bbl
+    INNER JOIN rentstab rentstab on rentstab.ucbbl = dobjobs.bbl
+    WHERE
+    pluto.cd = '${ cd }' 
+    AND latestactiondate >= date_trunc('month', current_date - interval '1 month') 
+    AND coalesce(uc2007,uc2008, uc2009, uc2010, uc2011, uc2012, uc2013, uc2014,uc2015,uc2016) is not null
+    group by dobjobs.bbl, pluto.cd, pluto.address, residentialunits, uc2007, uc2016, borocode, pluto.block, pluto.lot
+    ) as subset
+LEFT JOIN hpd_registrations_grouped_by_bbl_with_contacts hpd_reg on hpd_reg.bbl = subset.bbl
+group by subset.bbl, cd, address, residentialunits, uc2007, uc2016, a1, a2, dm, total, borocode, hpdlink, bislink, acrislink
+order by subset.bbl asc
+
+
 
--- dashboard/sql/hpd_violations.sql
+++ dashboard/sql/hpd_violations.sql
@@ -1,9 +1,32 @@
-select viols.bbl, 
-	pluto.cd, 
+SELECT subset.*,   
+	first(concat('<a href="http://whoownswhat.justfix.nyc/address/',
+       case 
+              when borocode = '1' then 'MANHATTAN'
+              when borocode = '2' then 'BRONX'
+              when borocode = '3' then 'BROOKLYN'
+              when borocode = '4' then 'QUEENS'
+              when borocode = '5' then 'STATEN ISLAND'
+       end,
+       '/',
+       split_part(address,' ',1),
+       '/',
+       split_part(address,' ',2),
+       '%20',
+       split_part(address,' ',3),
+       '%20',
+       split_part(address,' ',4),
+       '" target="_blank">',
+       replace(trim(both'"{}"' from cast(corpnames as text)), '"',''),
+        ' </a>'
+           )) as owner
+    from
+	(select pluto.cd,
+	viols.bbl,
 	pluto.address,
 	pluto.unitsres as residentialunits,
 	uc2007, uc2016,
-	corpnames,
+    pluto.borocode,
+
 	count(case when violationclass = 'A' then 1 else null end) as class_a,
 	count(case when violationclass = 'B' then 1 else null end) as class_b,
 	count(case when violationclass = 'C' then 1 else null end) as class_c,
@@ -12,26 +35,6 @@ select viols.bbl,
 		when violationclass = 'B' then 1
 		when violationclass = 'C' then 1
 		else 0 end) as total,
-      concat('<a href="http://whoownswhat.justfix.nyc/address/',
-            case 
-                  when pluto.borocode = '1' then 'MANHATTAN'
-                  when pluto.borocode = '2' then 'BRONX'
-                  when pluto.borocode = '3' then 'BROOKLYN'
-                  when pluto.borocode = '4' then 'QUEENS'
-                  when pluto.borocode = '5' then 'STATEN ISLAND'
-             end,
-            '/',
-            split_part(pluto.address,' ',1),
-            '/',
-            split_part(pluto.address,' ',2),
-            '%20',
-            split_part(pluto.address,' ',3),
-            '%20',
-            split_part(pluto.address,' ',4),
-            '" target="_blank">',
-            replace(trim(both'"{}"' from cast(hpd_reg.corpnames as text)), '"',''),
-            ' </a>'
-               ) as owner,
       concat('<a href="https://hpdonline.hpdnyc.org/HPDonline/Provide_address.aspx?p1=',
             pluto.borocode,
             '&p2=',
@@ -58,30 +61,24 @@ select viols.bbl,
             '&lot=',
             pluto.lot,
             '" target="_blank">(ACRIS)</a>'
-               ) as acrislink,
-      case when ((cast(uc2007 as float) - 
-                cast(uc2016 as float)) 
-               /cast(uc2007 as float) >= 0.25) then 0 else 1 end as highloss
-from hpd_violations viols
-left join pluto_16v2 pluto on pluto.bbl = viols.bbl
-inner join rentstab on rentstab.ucbbl=viols.bbl
-left join hpd_registrations_grouped_by_bbl_with_contacts hpd_reg on hpd_reg.bbl = viols.bbl
-where 
-	pluto.cd = '${ cd }' 
-	and novissueddate >= date_trunc('month', current_date - interval '2 month') 
-      and novissueddate < date_trunc('month', current_date - interval '1 month') 
-      and coalesce(uc2007,uc2008, uc2009, uc2010, uc2011, uc2012, uc2013, uc2014,uc2015,uc2016) is not null
-group by pluto.cd, 
-		 viols.bbl,  
-         pluto.address, 
-         pluto.unitsres, 
-         uc2007, 
-         uc2016, 
-         corpnames, 
-         pluto.borocode, 
-         pluto.block, 
-         pluto.lot
-having count(violationclass) > 9
-order by pluto.cd asc, total desc;
+               ) as acrislink
+	from hpd_violations viols
+    LEFT JOIN pluto_16v2 pluto on viols.bbl = pluto.bbl
+    INNER JOIN rentstab rentstab on rentstab.ucbbl = viols.bbl
+    WHERE
+       pluto.cd = '${ cd }' 
+	   and novissueddate >= date_trunc('month', current_date - interval '3 month')
+       and novissueddate < date_trunc('month', current_date - interval '2 month')
+       AND coalesce(uc2007,uc2008, uc2009, uc2010, uc2011, uc2012, uc2013, uc2014,uc2015,uc2016) is not null
+    group by viols.bbl, pluto.cd, pluto.address, residentialunits, novissueddate, uc2007, uc2016, borocode, pluto.block, pluto.lot
+    having count(violationclass) > 9
+    ) as subset
+LEFT JOIN hpd_registrations_grouped_by_bbl_with_contacts hpd_reg on hpd_reg.bbl = subset.bbl
+group by subset.bbl, cd, address, residentialunits, uc2007, uc2016, class_a, class_b, class_c, total, borocode, hpdlink, bislink, acrislink
+order by subset.bbl asc
+
+
+
+
 
 
