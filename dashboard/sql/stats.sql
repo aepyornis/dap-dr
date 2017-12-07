cee22a93cdffc25c2573696102290bda201e9d1d
ziggy <ziggy@elephant-bird.net>
2017-12-07 07:45:31 -0500

    update stats open violations query

--- dashboard/sql/stats.sql
+++ dashboard/sql/stats.sql
@@ -21,7 +21,8 @@ select	'buildingsWithOpenViolations' as name,
 	count(distinct ov_bbls.*) as d
 from (
 	select distinct bbl as bbl
-     	from hpd_open_violations
+     	from hpd_violations
+	where currentstatusid not in (3,6,9,19,36)
 ) as ov_bbls
 inner join pluto_16v2 on pluto_16v2.bbl = ov_bbls.bbl
 where pluto_16v2.cd = '${ cd }'
@@ -32,7 +33,8 @@ select 'totalNumberOfOpenViolations' as name,
        sum(ov.c) as d
 from (
 	select bbl, count(*) as c
-     	from hpd_open_violations
+     	from hpd_violations
+	where currentstatusid not in (3,6,9,19,36)
     	group by bbl
       ) as ov
 left join pluto_16v2 on pluto_16v2.bbl = ov.bbl
