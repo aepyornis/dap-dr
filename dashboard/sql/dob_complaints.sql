6cc6ec48fa959ddf21539695b59b370375629c10
lblok <Lucy@Nathan-Bridgess-iMac-6.local>
2018-08-15 16:19:07 -0400

    sql changes and translate feature

--- dashboard/sql/dob_complaints.sql
+++ dashboard/sql/dob_complaints.sql
@@ -1,92 +1,42 @@
 select subset.*,
-      first(replace(trim(both'"{}",' from cast(corpnames as text)), '"','')) as ownertext,
-      first(concat('<a href="http://whoownswhat.justfix.nyc/address/',
-            case 
-                  when borocode = '1' then 'MANHATTAN'
-                  when borocode = '2' then 'BRONX'
-                  when borocode = '3' then 'BROOKLYN'
-                  when borocode = '4' then 'QUEENS'
-                  when borocode = '5' then 'STATEN ISLAND'
-            end,
-            '/',
-            split_part(address,' ',1),
-            '/',
-            split_part(address,' ',2),
-            '%20',
-            split_part(address,' ',3),
-            '%20',
-            split_part(address,' ',4),
-            '" target="_blank">',
-            replace(trim(both'"{}",' from cast(corpnames as text)), '"',''),
-            ' </a>'
-            )) as owner
+      first(replace(trim(both'"{}",' from cast(corpnames as text)), '"','')) as owner
       from
-      (select communityboard, 
-      pluto.bbl, 
-      concat(housenumber,' ',housestreet) as address,
-      pluto.unitsres as residentialunits, 
-      uc2007, uc2016,
-      pluto.borocode,
-      pluto.zipcode,
-      pluto.council,
-      count(distinct complaintnumber) as dobcomplaints,
-            concat('<a href="https://hpdonline.hpdnyc.org/HPDonline/Provide_address.aspx?p1=',
-                  pluto.borocode,
-                  '&p2=',
-                  split_part(pluto.address,' ', 1),
-                  '&p3=',
-                  split_part(pluto.address,' ',2),
-                  '+',
-                  split_part(pluto.address,' ',3),
-                  '+',
-                  split_part(pluto.address,' ',4),
-                  '" target="_blank">(HPD)</a>'
-                  ) as hpdlink,
-            concat('<a href="http://a810-bisweb.nyc.gov/bisweb/PropertyProfileOverviewServlet?boro=',
-                  pluto.borocode,
-                  '&block=',
-                  pluto.block,
-                  '&lot=',
-                  pluto.lot,
-                  '" target="_blank">(DOB)</a>') as bislink,
-            concat('<a href="http://a836-acris.nyc.gov/bblsearch/bblsearch.asp?borough=',
-                  pluto.borocode,
-                  '&block=',
-                  pluto.block,
-                  '&lot=',
-                  pluto.lot,
-                  '" target="_blank">(ACRIS)</a>'
-                  ) as acrislink,
-            concat('<a href="http://webapps.nyc.gov:8084/CICS/fin1/find001i?FFUNC=C&FBORO=',
-                  pluto.borocode,
-                  '&FBLOCK=',
-                  pluto.block,
-                  '&FLOT=',
-                  pluto.lot,
-                  '" target="_blank">(Taxes)</a>') as taxlink,
-            concat('<a href="http://www.oasisnyc.net/map.aspx?zoomto=lot:',
-                  pluto.bbl,
-                  '" target="_blank">(OASIS)</a>') as oasislink,
-            concat('<a href="http://www.google.com/maps/place/',
-                  pluto.address,
-                  ' ',
-                  pluto.zipcode,
-                  '" target="_blank">',
-                  pluto.address,
-                  '</a>') as googlelink
+            (select communityboard, 
+            pluto.bbl, 
+            concat(housenumber,' ',housestreet) as address,
+            pluto.unitsres as residentialunits, 
+            uc2007, uc2016,
+            pluto.borocode,
+            pluto.block,
+            pluto.lot,
+            pluto.zipcode,
+            pluto.council,
+            count(distinct complaintnumber) as dobcomplaints,
+            concat('https://hpdonline.hpdnyc.org/HPDonline/Provide_address.aspx?p1=',
+            pluto.borocode,
+            '&p2=',
+            split_part(pluto.address,' ', 1),
+            '&p3=',
+            split_part(pluto.address,' ',2),
+            '+',
+            split_part(pluto.address,' ',3),
+            '+',
+            split_part(pluto.address,' ',4)) as hpdlink
       from dob_complaints dob
-      left join pluto_17v1 pluto on pluto.address = concat(housenumber,' ',housestreet)
+      left join pluto_18v1 pluto on pluto.address = concat(housenumber,' ',housestreet)
       inner join rentstab on rentstab.ucbbl=pluto.bbl
       where 
       communityboard = '${ cd }'
-      and dateentered between '06-01-2018' and '06-30-2018'
+      and dateentered >= date_trunc('month', current_date - interval '1 month') 
+      and dateentered < date_trunc('month', current_date - interval '0 month')
       and pluto.unitsres > 0 
       AND COALESCE(uc2007,uc2008, uc2009, uc2010, uc2011, uc2012, uc2013, uc2014,uc2015,uc2016) is not null
       group by pluto.bbl, concat(housenumber,' ',housestreet), communityboard, pluto.unitsres, uc2007, uc2016, pluto.address, pluto.borocode, pluto.block, pluto.lot, pluto.council, pluto.zipcode, pluto.bbl
       having count(distinct complaintnumber) > 1
       ) as subset
 LEFT JOIN hpd_registrations_grouped_by_bbl_with_contacts hpd_reg on hpd_reg.bbl = subset.bbl
-group by subset.bbl, address, communityboard, residentialunits, uc2007, uc2016, dobcomplaints, borocode, hpdlink, bislink, acrislink, googlelink, taxlink, oasislink, council, zipcode
+left join pluto_18v1 pluto on pluto.bbl = subset.bbl
+group by subset.bbl, subset.address, communityboard, residentialunits, uc2007, uc2016, dobcomplaints, subset.borocode, subset.block, subset.lot, hpdlink, subset.council, subset.zipcode
       order by communityboard asc, dobcomplaints desc
 
 
--- dashboard/sql/dob_jobs.sql
+++ dashboard/sql/dob_jobs.sql
@@ -3,31 +3,19 @@ create or replace view bbljobs as
            job,
            jobtype
     from dobjobs
-    where prefilingdate between '06-01-2018' and '06-30-2018'
+    where prefilingdate >= date_trunc('month', current_date - interval '1 month') 
+	and prefilingdate < date_trunc('month', current_date - interval '0 month')
     group by bbl, job, jobtype;
 
 SELECT subset.*,   
       first(replace(trim(both'"{}",' from cast(corpnames as text)), '"','')) as ownertext,
-      first(concat('<a href="http://whoownswhat.justfix.nyc/address/',
-            case 
-                  when borocode = '1' then 'MANHATTAN'
-                  when borocode = '2' then 'BRONX'
-                  when borocode = '3' then 'BROOKLYN'
-                  when borocode = '4' then 'QUEENS'
-                  when borocode = '5' then 'STATEN ISLAND'
-            end,
-            '/',
-            split_part(address,' ',1),
-            '/',
-            split_part(address,' ',2),
-            '%20',
-            split_part(address,' ',3),
-            '%20',
-            split_part(address,' ',4),
+      first(concat('<a href="http://whoownswhat.justfix.nyc/bbl/',
+            pluto.borocode,'/',
+            pluto.block,'/',
+            pluto.lot,
             '" target="_blank">',
             replace(trim(both'"{}",' from cast(corpnames as text)), '"',''),
-            ' </a>'
-            )) as owner
+            ' </a>')) as owner
     from
 	(select pluto.cd,
 	bbljobs.bbl,
@@ -35,6 +23,8 @@ SELECT subset.*,
 	pluto.unitsres as residentialunits,
 	uc2007, uc2016,
     pluto.borocode,
+    pluto.block,
+    pluto.lot,
     pluto.zipcode,
     pluto.council,
     count(case when bbljobs.jobtype ='A1' then 1 else null end) as a1,
@@ -45,7 +35,7 @@ SELECT subset.*,
         when bbljobs.jobtype = 'A2' then 1
         when bbljobs.jobtype = 'DM' then 1
         else 0 end) as total,
-        concat('<a href="https://hpdonline.hpdnyc.org/HPDonline/Provide_address.aspx?p1=',
+            concat('https://hpdonline.hpdnyc.org/HPDonline/Provide_address.aspx?p1=',
             pluto.borocode,
             '&p2=',
             split_part(pluto.address,' ', 1),
@@ -54,43 +44,9 @@ SELECT subset.*,
             '+',
             split_part(pluto.address,' ',3),
             '+',
-            split_part(pluto.address,' ',4),
-            '" target="_blank">(HPD)</a>'
-               ) as hpdlink,
-        concat('<a href="http://a810-bisweb.nyc.gov/bisweb/PropertyProfileOverviewServlet?boro=',
-            pluto.borocode,
-            '&block=',
-            pluto.block,
-            '&lot=',
-            pluto.lot,
-            '" target="_blank">(DOB)</a>') as bislink,
-        concat('<a href="http://a836-acris.nyc.gov/bblsearch/bblsearch.asp?borough=',
-            pluto.borocode,
-            '&block=',
-            pluto.block,
-            '&lot=',
-            pluto.lot,
-            '" target="_blank">(ACRIS)</a>'
-               ) as acrislink,
-        concat('<a href="http://webapps.nyc.gov:8084/CICS/fin1/find001i?FFUNC=C&FBORO=',
-            pluto.borocode,
-            '&FBLOCK=',
-            pluto.block,
-            '&FLOT=',
-            pluto.lot,
-            '" target="_blank">(Taxes)</a>') as taxlink,
-        concat('<a href="http://www.oasisnyc.net/map.aspx?zoomto=lot:',
-            pluto.bbl,
-            '" target="_blank">(OASIS)</a>') as oasislink,
-        concat('<a href="http://www.google.com/maps/place/',
-            pluto.address,
-            ' ',
-            pluto.zipcode,
-            '" target="_blank">',
-            pluto.address,
-            '</a>') as googlelink
+            split_part(pluto.address,' ',4)) as hpdlink
 	from bbljobs
-    LEFT JOIN pluto_17v1 pluto on bbljobs.bbl = pluto.bbl
+    LEFT JOIN pluto_18v1 pluto on bbljobs.bbl = pluto.bbl
     INNER JOIN rentstab rentstab on rentstab.ucbbl = bbljobs.bbl
     WHERE
         pluto.cd = '${ cd }' 
@@ -98,9 +54,10 @@ SELECT subset.*,
     group by bbljobs.bbl, pluto.cd, pluto.address, residentialunits, uc2007, uc2016, borocode, pluto.block, pluto.lot, pluto.council, pluto.zipcode, pluto.bbl
     ) as subset
 LEFT JOIN hpd_registrations_grouped_by_bbl_with_contacts hpd_reg on hpd_reg.bbl = subset.bbl
+left join pluto_18v1 pluto on pluto.bbl = subset.bbl
 where (
     a1 > 0 or
     a2 > 0 or
     dm > 0)
-group by subset.bbl, cd, address, residentialunits, uc2007, uc2016, a1, a2, dm, total, borocode, hpdlink, bislink, acrislink, googlelink, taxlink, oasislink, council, zipcode
+group by subset.bbl, subset.cd, subset.address, residentialunits, uc2007, uc2016, a1, a2, dm, total, subset.borocode, subset.block, subset.lot, hpdlink,  subset.council, subset.zipcode
 order by cd asc, a2 desc
