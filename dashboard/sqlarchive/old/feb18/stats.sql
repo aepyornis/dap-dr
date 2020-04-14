b1343bd97fcaa078659dcc6efdc24346d4ac9dde
Lucy Block <lucy@Lucys-MacBook-Pro.local>
2020-04-14 14:08:40 -0400

    fix json and page build errors

--- dashboard/sqlarchive/old/feb18/stats.sql
+++ dashboard/sqlarchive/old/feb18/stats.sql
@@ -1,17 +1,17 @@
 select 'lots' as name, count(*) as d
-from pluto_16v2
+from pluto_18v2
 where cd = '${ cd }'
 
 union
 
 select 'unitsres' as name, sum(unitsres) as d
-from pluto_16v2
+from pluto_18v2
 where cd = '${ cd }'
 
 union
 
 select 'buildingsres' as name, sum(numbldgs) as d
-from pluto_16v2
+from pluto_18v2
 where cd = '${ cd }'
 and unitsres > 0
 
@@ -24,8 +24,8 @@ from (
      	from hpd_violations
 	where currentstatusid not in (3,6,9,19,36)
 ) as ov_bbls
-inner join pluto_16v2 on pluto_16v2.bbl = ov_bbls.bbl
-where pluto_16v2.cd = '${ cd }'
+inner join pluto_18v2 on pluto_18v2.bbl = ov_bbls.bbl
+where pluto_18v2.cd = '${ cd }'
 
 union
 
@@ -37,5 +37,5 @@ from (
 	where currentstatusid not in (3,6,9,19,36)
     	group by bbl
       ) as ov
-left join pluto_16v2 on pluto_16v2.bbl = ov.bbl
-where pluto_16v2.cd = '${ cd }';
+left join pluto_18v2 on pluto_18v2.bbl = ov.bbl
+where pluto_18v2.cd = '${ cd }';
